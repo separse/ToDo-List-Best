55121889c05cc8b93b47e4712424fdbdfb2b26e7
separse <sepehrroshanpour@gmail.com>
2021-01-06 11:06:15 +0330

    structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { ITodo } from './todo-interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sepehr-roshanpour-todo';
+  title: string = '';
+  todo: ITodo;
+
+  todos: ITodo[] = [
+    {
+      title: 'go running',
+      done: false,
+      edited: true,
+    },
+    {
+      title: 'call coach',
+      done: false,
+      edited: true,
+    },
+    {
+      title: 'do homework',
+      done: false,
+      edited: true,
+    },
+  ];
+
+  addTodo() {
+    if (this.title) {
+      this.todo = {
+        title: this.title,
+        done: false,
+        edited: true,
+      };
+      this.todos.push(this.todo);
+      this.title = '';
+    }
+  }
+
+  toggleCheck(todo) {
+    todo.done = !todo.done;
+  }
+
+  editTodo(todo) {
+    todo.edited = !todo.edited;
+  }
+
+  deleteTodo(i) {
+    this.todos.splice(i, 1);
+  }
 }
